365e4729046826656b6c154f6f703f25f20dffac
Veronika Muminova <muminova.veronika@gmail.com>
2021-06-25 21:40:28 +0400

    fix html, css, order of variables in js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const popupElement = document.querySelector(".popup");
 const popupClose = document.querySelector(".popup__close");
 const popupOpen = document.querySelector(".profile__edit-button");
+let formElement = document.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".popup__input_type_name").value;
+const jobInput = formElement.querySelector(".popup__input_type_job").value;
+let title = document.querySelector(".profile__title");
+let subtitle = document.querySelector(".profile__subtitle");
+
 
 const openPopup = function () {
     popupElement.classList.add("popup_opened");
@@ -10,25 +16,14 @@ const closePopup = function () {
     popupElement.classList.remove("popup_opened");
 };
 
-popupOpen.addEventListener("click", openPopup);
-popupClose.addEventListener("click", closePopup);
-
-// Находим форму в DOM
-let formElement = document.querySelector(".formElement");
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    // Находим поля формы в DOM
-    const nameInput = formElement.querySelector(".popup__input-name").value;
-    const jobInput = formElement.querySelector(".popup__input-job").value;
 
-    let title = document.querySelector(".profile__title");
-    let subtitle = document.querySelector(".profile__subtitle");
-
-    title.textContent = nameInput;
-    subtitle.textContent = jobInput;
+   title.textContent = nameInput;
+   subtitle.textContent = jobInput;
 
     closePopup();
 }
@@ -36,3 +31,6 @@ function formSubmitHandler(evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler);
+
+popupOpen.addEventListener("click", openPopup);
+popupClose.addEventListener("click", closePopup);
